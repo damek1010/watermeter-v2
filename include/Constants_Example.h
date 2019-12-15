c107485c4ba97bb74fb106b4c75f4b2fc4a74238
Damian <jdequo@gmail.com>
2019-12-15 14:16:19 +0100

    Added constants file and saving measurements function

---
+++ include/Constants_Example.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define PIN_CLK 14
+#define PIN_MISO 12
+#define PIN_MOSI 13
+#define PIN_CS 15
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,14 @@
 #include "ESP8266WiFi.h"
 #include <WiFiUdp.h>
 #include <NTPClient.h>
- 
-const char* ssid     = "912B";
-const char* password = "splot123";
+#include "Constants.h"
+#include <SD.h>
 
-struct Measurement {
+const char *ssid = "912B";
+const char *password = "splot123";
+
+struct Measurement
+{
   int value;
   String time;
 };
@@ -15,16 +18,59 @@ WiFiUDP ntpUDP;
 
 NTPClient timeClient(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
 
-void saveMeasurement(int value) {
+File csv;
+
+String getCurrentDate()
+{
+  time_t rawtime = timeClient.getEpochTime();
+  struct tm ts = *localtime(&rawtime);
+  char buf[80];
+  strftime(buf, sizeof(buf), "%Y-%m-%d", &ts);
+  Serial.println(buf);
+
+  return buf;
+}
+
+void saveMeasurement(int value)
+{
+  /*
+  *
+  * Wczytywanie czasu
+  * 
+  */
+
   timeClient.update();
+
+  /*
+  *
+  * Tworzenie pomiaru
+  * 
+  */
   Measurement measurement;
   measurement.time = timeClient.getFormattedTime();
   measurement.value = value;
-  Serial.print("Saving measurement: {\n\tvalue:");// +  +  + "\n}"
+
+  /*
+  *
+  * Zapisywanie pomiaru
+  * 
+  */
+  Serial.print("Saving measurement: {\n\tvalue:");
   Serial.print(measurement.value);
   Serial.print(",\n\ttime: ");
   Serial.print(measurement.time);
   Serial.print("\n}");
+
+  String filename = String(getCurrentDate());
+  filename.concat(".csv");
+  csv = SD.open(filename, FILE_WRITE);
+
+  String buf = String(measurement.value);
+  buf.concat(", ");
+  buf.concat(measurement.time);
+
+  csv.println(buf);
+  csv.close();
 }
 
 void setup()
@@ -32,21 +78,31 @@ void setup()
   Serial.begin(115200);
   delay(10);
 
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(PIN_CS))
+  {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+
   WiFi.begin(ssid, password);
-  
-  while (WiFi.status() != WL_CONNECTED) {
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
 
   Serial.println("");
-  Serial.println("WiFi connected");  
+  Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
   timeClient.begin();
 }
- 
+
 void loop()
 {
   saveMeasurement(10);

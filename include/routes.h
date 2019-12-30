3e5edffd5eea8c904636d5059908ae06efb3a086
Damian <jdequo@gmail.com>
2019-12-30 21:44:04 +0100

    Added bootstrap and JS handler

--- include/routes.h
+++ include/routes.h
@@ -117,8 +117,19 @@ void handleSettings()
     sendFile("/web/settings.html");
 }
 
+void handleJS(String path) {
+    String fullpath = "/web" + path;
+    sendFile(fullpath);
+}
+
 void handleNotFound()
 {
+    String path = server.uri();
+    int index = path.indexOf('/js');
+    if (index > 0) {
+
+    }
+
     server.send(404, "text/html", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
 }
 
